4abfe02af9bcc80ba1d3ab57a2ed8bc50ee65970
Pasindu Dilshan Dimbulana <pasindudi@mobitel.lk>
2024-11-22 16:53:25 +0530

    add around

--- src/main/java/com/aop/aop/LoggingAspect.java
+++ src/main/java/com/aop/aop/LoggingAspect.java
@@ -12,25 +12,25 @@ public class LoggingAspect {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Before("execution(* com.aop.*.*.*(..))")
-    public void beforeLogMethodCall(JoinPoint joinPoint){
-        logger.info("Before Aspect - method is called : {} : .................... : {}", joinPoint, joinPoint.getArgs());
-    }
-
-    @After("execution(* com.aop.*.*.*(..))")
-    public void afterLogMethodCall(JoinPoint joinPoint){
-        logger.info("After Aspect - method is called : {}", joinPoint);
-    }
-
-    @AfterThrowing(pointcut = "execution(* com.aop.*.*.*(..))", throwing = "exception")
-    public void afterThrowingLogMethodCall(JoinPoint joinPoint, Exception exception){
-        logger.info("After Throwing Aspect - method is called : {}", joinPoint);
-        logger.warn("After Throwing Aspect - Exception is : {}", exception.toString());
-    }
-
-    @AfterReturning(pointcut = "execution(* com.aop.*.*.*(..))" , returning = "returnValue")
-    public void afterReturningLogMethodCall(JoinPoint joinPoint, Object returnValue){
-        logger.info("After Returning Aspect - method is called : {}", joinPoint);
-        logger.info("After Returning Aspect - Return value is : {}", returnValue.toString());
-    }
+//    @Before("execution(* com.aop.*.*.*(..))")
+//    public void beforeLogMethodCall(JoinPoint joinPoint){
+//        logger.info("Before Aspect - method is called : {} : .................... : {}", joinPoint, joinPoint.getArgs());
+//    }
+//
+//    @After("execution(* com.aop.*.*.*(..))")
+//    public void afterLogMethodCall(JoinPoint joinPoint){
+//        logger.info("After Aspect - method is called : {}", joinPoint);
+//    }
+//
+//    @AfterThrowing(pointcut = "execution(* com.aop.*.*.*(..))", throwing = "exception")
+//    public void afterThrowingLogMethodCall(JoinPoint joinPoint, Exception exception){
+//        logger.info("After Throwing Aspect - method is called : {}", joinPoint);
+//        logger.warn("After Throwing Aspect - Exception is : {}",  exception.toString());
+//    }
+//
+//    @AfterReturning(pointcut = "execution(* com.aop.*.*.*(..))" , returning = "returnValue")
+//    public void afterReturningLogMethodCall(JoinPoint joinPoint, Object returnValue){
+//        logger.info("After Returning Aspect - method is called : {}", joinPoint);
+//        logger.info("After Returning Aspect - Return value is : {}", returnValue.toString());
+//    }
 }
---
+++ src/main/java/com/aop/aop/PerformanceTrackingAspect.java
@@ -0,0 +1,33 @@
+package com.aop.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class PerformanceTrackingAspect {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Around("execution(* com.aop.service.*.*(..))")
+    public Object findExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+        logger.info("Around Aspect - Starts execute : {}", startTime);
+
+        Object returnValue = joinPoint.proceed();
+        logger.info("Around Aspect - Processing");
+
+        long endTime = System.currentTimeMillis();
+        logger.info("Around Aspect - End execute : {}", endTime);
+
+        long executionDuration = endTime - startTime;
+
+        logger.info("Around Aspect : {} - execution duration : {}", joinPoint, executionDuration);
+        return returnValue;
+    }
+}
